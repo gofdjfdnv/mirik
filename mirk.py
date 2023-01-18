fce985370c1f7302c3bcc53cb1f12045c519ba49
Orel <95867547+gofdjfdnv@users.noreply.github.com>
2023-01-18 17:14:05 +0200

    Create mirk.py

---
+++ mirk.py
@@ -0,0 +1,109 @@
+def func2(arg20, arg21):
+    if arg21 < arg21:
+        var26 = class3()
+    else:
+        var26 = class5()
+    for var27 in xrange(23):
+        var28 = var26.func4
+        var28(arg21, arg21)
+    var32 = func7(arg21, arg20)
+    var37 = func8(arg21, var32)
+    var46 = var40(var32, var37)
+    var47 = func14()
+    if arg20 < var47:
+        var48 = (((var32 | (507 ^ -930 | (var32 - var46 & -701049460)) + var37) | arg21 | -209951877) & -911709272 | -1168980753 + var46) & var47 | (arg20 | ((arg21 + var37) - arg20 | (var37 - (-653 & arg21 + arg20)))) & arg20
+    else:
+        var48 = (var46 + (var47 - var47) - 300 + var46 & arg21) + -1021303293
+    result = arg20 - var47 - var46
+    return result
+def func14():
+    func12()
+    result = len(xrange(14))
+    func13()
+    return result
+def func13():
+    global len
+    del len
+def func12():
+    global len
+    len = lambda x : 9
+def func11(arg41, arg42):
+    if arg41 < arg42:
+        var43 = arg41 & (arg41 + arg41 & arg42)
+    else:
+        var43 = arg41 ^ (arg41 | -851 | 455 | -868 & -441 | 988 | (arg42 + arg41) | 142638513) ^ 1629364256 & 69
+    var44 = (arg42 ^ arg41 - (316 + (arg41 | (491396658 + arg42 ^ (784378849 | arg42 ^ (((-1879018879 - (375 - -766481668 & -1803294222)) & (arg42 | arg41 & arg41 - arg41 ^ 744988938 - arg41 | -134) ^ arg42) | arg42)))))) + arg42
+    var45 = (arg42 - -1224234623 ^ 217138857 - arg41) | -83
+    result = (var45 | var45 - arg41 + (((arg41 - arg41) & 262 | arg41) ^ arg41 ^ var45) ^ 1931540296) & var45 + arg42
+    return result
+def func10():
+    closure = [-10]
+    def func9(arg38, arg39):
+        closure[0] += func11(arg38, arg39)
+        return closure[0]
+    func = func9
+    return func
+var40 = func10()
+def func8(arg33, arg34):
+    var35 = 0
+    for var36 in range(29):
+        var35 += (8 & var35) & arg33
+    return var35
+def func7(arg29, arg30):
+    if arg30 < arg29:
+        var31 = -629 + 711 ^ arg30 & 176 & (arg29 + arg30 ^ 720 ^ (((334 - ((756395269 - arg29) + -493)) + arg29 - arg29 + 1846761503) & -773 & arg30 | -530) + arg29 - (arg29 | 297) & -343) - -461
+    else:
+        var31 = (arg30 | (703 ^ 169)) | arg30 | arg30
+    result = 734 - -251 | (-900 ^ arg29 & arg29) + 266276887 + 284
+    return result
+class class5(object):
+    def func4(self, arg24, arg25):
+        return 0
+class class3(class5):
+    def func4(self, arg22, arg23):
+        result = ((arg23 | (-1 | arg22) | 0) + arg23) + 405915073 | arg22
+        return result
+def func1(arg1, arg2):
+    var3 = arg2 | -2078907749
+    var4 = 1473285831 & (arg2 | arg2 | arg2)
+    var5 = arg1 - (var3 ^ arg2) ^ var4
+    var6 = arg1 | var3 | -2121169673 & 2141324745
+    var7 = (-588507590 | var5) ^ -543
+    var8 = var7 + var5 ^ var7 - var5
+    var9 = var5 & var3 | arg2 & 744662724
+    var10 = (arg2 + 269 - var7) ^ var3
+    if var10 < arg1:
+        var11 = var3 & var3 - 727 ^ 36
+    else:
+        var11 = var10 - var6 - var7
+    if var7 < var7:
+        var12 = var5 | var4
+    else:
+        var12 = var10 ^ 294 & -655 & var7
+    var13 = ((var7 & var4) & var6) ^ var4
+    var14 = (var10 ^ arg1 - var10) | var10
+    var15 = (648 - var4) + var9
+    var16 = (var15 & var13) & 618
+    if var7 < var15:
+        var17 = var4 | (54425791 | -858842640)
+    else:
+        var17 = (var7 & var13 ^ var16) + var5
+    var18 = var3 & 969 | (var13 | var6)
+    var19 = (var18 + -1907908652) | var10 & var8
+    result = 644 - (((var5 & var5) + var7 & var7 | (var3 & var18 & var18 + var7 | var9)) | arg1 & var3)
+    return result
+if __name__ == "__main__":
+    print 'prog_size: 0'
+    print 'func_number: 2'
+    print 'arg_number: 20'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
+    print 'prog_size: 5'
+    print 'func_number: 15'
+    print 'arg_number: 49'
+    for i in xrange(25000):
+        x = 5
+        x = func2(x, i)
+        print x,
